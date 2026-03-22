7ad418f740d2b79576ad8b29fa27a19bcd0ae66e
ben-ben2018 <41539827+ben-ben2018@users.noreply.github.com>
2026-03-23 02:14:29 +0800

    chore: rename site to 笨驴 (Dumb Donkey)

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '笨驴 | Dumb Donkey';
+export const SITE_DESCRIPTION = '笨驴（Dumb Donkey）的个人博客。';
